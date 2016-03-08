0f00744d22142cf6ef0ff4a5afe29d0214699489
Larry Clem <larryvincentclem@gmail.com>
2016-03-08 17:35:51 -0600

    done

--- server.js
+++ server.js
@@ -1,1 +1,52 @@
+var express = require('express');
+
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use (bodyParser.json());
+
+app.listen (9999, function (){
+	console.log('Listening on 9999, asshole.');
+});
+
+var messages = [];
+
+app.get('/', function (req, res){
+	  res.status(200).set({
+	    'Content-Type': 'application/json',
+	    'Access-Control-Allow-Origin': '*',
+	    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+	    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+	    'X-XSS-Protection': '1; mode=block',
+	    'X-Frame-Options': 'SAMEORIGIN',
+	    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+	}).send(JSON.stringify (messages));	
+});
+
+app.post('/', function (req, res){
+	console.log(req.body);
+	messages.push({
+		message: req.body.message,
+		time: new Date()
+	});//ask about this.
+	res.status(200).set({
+		'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
+
+app.options('/', function (req, res){
+	res.status(200).set({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+	}).send();
+});
+
 
